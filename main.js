00fd82a90cfae290f82551ea67ceea74132b4d44
gus3000 <gus3000spam@gmail.com>
2017-10-04 17:14:09 +0000

    video starting at correct time on launch; removed node-omxplayer

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const spawn = require('child_process').spawn;
 const PlugAPI = require('plugapi');
 const youtubedl = require('youtube-dl');
 const omx = require('node-omxplayer');
@@ -11,37 +12,46 @@ bot.connect('survival-machines'); // The part after https://plug.dj
 
 bot.on(PlugAPI.events.ROOM_JOIN, (room) => {
     console.log(`Joined ${room}`);
-    moteur();
+    main();
 });
 bot.on(PlugAPI.events.CHAT, (data) => {
     console.log(`${data.from} said ${data.message}`);
 });
 
-moteur = function() {
+function main()
+{
     bot.sendChat('Coucou, je suis le bot de td8 !');
-    media = bot.getMedia();
-    if (!media)
-    {
-	console.log('pas de media. Je ne sais pas quoi faire. Je panique.');
-	console.log('segmentation fault - core dumped');
-	process.exit();
-    }
-    cid = media.cid;
+    catchCurrentVideo(bot);
+}
+
+function catchCurrentVideo(bot)
+{
+    var timeElapsed = bot.getTimeElapsed();
+    var cid = bot.getMedia().cid;
+    
+    console.log('video current time : ' + timeElapsed);
+    readVideo(bot, cid, timeElapsed);
+}
+
+function readVideo(bot,cid,time=0)
+{
+    console.log('read ' + cid);
     var video = youtubedl(cid, ['-f mp4'], {cwd: __dirname});
     video.on('info', function(info) {
-	console.log('video started : ' + JSON.stringify(info, null, 4));
-	stream(info.url);
+	console.log('video started : ' + cid);
+
+	if (time != 0)
+	{
+	    stream(bot, info.url, bot.getTimeElapsed());
+	}
     });
 }
 
-stream = function(url)
+function stream(bot,url, time=0)
 {
-    var player = omx(url);
-    player.on('error', function(error) {
-	console.log('error :' + error);
+    console.log('stream ' + bot.getMedia());
+    let omxProcess = spawn('omxplayer', ['-l '+ time, url]);
+    omxProcess.on('close', function() {
+	console.log('player closed');
     });
-    player.on('close', function() {
-	console.log('closed !');
-    });
-    console.log('coucou');
 }
